f1eb95cca53891a9f417899303a9cb16138f2808
scorpioswagg <astro.butt84@gmail.com>
2025-04-19 01:48:30 -0700

    Update StarfieldHero.tsx

--- components/StarfieldHero.tsx
+++ components/StarfieldHero.tsx
@@ -1,6 +1,6 @@
-"use client";
-import React, { useEffect, useRef } from 'react';
-
++ "use client";
+  import React, { useEffect, useRef } from 'react';
+  
 interface Star {
   x: number;
   y: number;
